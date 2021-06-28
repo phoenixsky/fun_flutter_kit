2f0db64cda5bca89f3e96e19c7330e656620aac2
phoenixsky <moran.fc@gmail.com>
2021-06-28 13:27:16 +0800

    feature(export): 导包

--- lib/fun_flutter_kit.dart
+++ lib/fun_flutter_kit.dart
@@ -1,7 +1,4 @@
 library fun_flutter_kit;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'ext/fun_ext.dart';
+export 'state/fun_state.dart';
